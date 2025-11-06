ce3ff201905fbeee63349a061fc8d5fbbe2b55c6
KLM <91488137+planeklm@users.noreply.github.com>
2025-11-06 17:46:37 +0000

    feat: flags

--- main.go
+++ main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
 )
 
-func main() {
-	provisionFile := "345345355.mobileprovision"
+func checkProvision(directory string) bool {
+	provisionFile := directory
 
 	data, err := os.ReadFile(provisionFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pattern := `<key>PPQCheck</key>\s*<(true|false)/?`
+	key := `<key>PPQCheck</key>\s*<(true|false)/?`
 
-	re := regexp.MustCompile(pattern)
+	re := regexp.MustCompile(key)
 	match := re.FindString(string(data))
 
 	if match != "" {
-		println("ppqcheck")
+		return true
 	} else {
-		println("no ppqcheck")
+		return false
+	}
+}
+
+func main() {
+	directory := flag.String("directory", "", "Directory of the .mobileprovision (required)")
+	flag.Parse()
+
+	if *directory == "" {
+		fmt.Println("error: --directory flag is needed")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	ppqCheck := checkProvision(*directory)
+
+	if ppqCheck == true {
+		println("PPQ has been detected!")
+		return
+	} else {
+		println("PPQ has not been detected!")
+		return
 	}
 }
